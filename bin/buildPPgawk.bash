c58ec503437fd32e54f039f04d593a5a1a509f65
Andrew Moss <awm@bth.se>
2013-04-18 16:32:17 +0200

    Removed regexec from the gawk testcase.
    This is not a top-level translation unit, it gets included from another
    file so the internal_function attributes get removed by the preprocessor.
    Left the extra sed step in place to flush out any that get through by
    another route.

--- bin/buildPPgawk.bash
+++ bin/buildPPgawk.bash
@@ -3,6 +3,9 @@ mkdir -p testcases/pp-gawk
 pushd testcases/pp-gawk
 # No -save-temps=cwd option on BSD. 
 for t in ../gawk-4.0.2/*.c ; do gcc -c -save-temps -DDEFPATH='".:/usr/local/share/awk"' -DHAVE_CONFIG_H -DGAWK -DLOCALEDIR='"/usr/local/share/locale"' -I../gawk-4.0.2 $t ; done
+# Not a top-level unit, included from regex.c. Needs right flags to preprocess out the GCC function attribute crap.
+rm regexec.*
 # Explicit zero-length extension also works on BSD, but needs to know the command is not a fucked up attempt at an extension.
 sed -i -e 's/^# \([0-9]*\)/#line \1/' *.i
+sed -i -e 's/internal_function//' *.i
 popd
