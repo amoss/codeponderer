12fe909d21dfe7c5755395305d73d3f8b3949178
Andrew Moss <awm@bth.se>
2013-04-19 14:33:49 +0200

    Removed more units that were not valid targets.
    Only remaining lexx error in gawk is the old-style K&R definition
    in random. Will ignore that one so next up is trapping all of the
    tree-parsing / semantic analysis errors to build a second status.

--- bin/buildPPgawk.bash
+++ bin/buildPPgawk.bash
@@ -5,6 +5,8 @@ pushd testcases/pp-gawk
 for t in ../gawk-4.0.2/*.c ; do gcc -c -save-temps -DDEFPATH='".:/usr/local/share/awk"' -DHAVE_CONFIG_H -DGAWK -DLOCALEDIR='"/usr/local/share/locale"' -I../gawk-4.0.2 $t ; done
 # Not a top-level unit, included from regex.c. Needs right flags to preprocess out the GCC function attribute crap.
 rm regexec.*
+rm regcomp.*
+rm regex_internal.*
 # Explicit zero-length extension also works on BSD, but needs to know the command is not a fucked up attempt at an extension.
 sed -i -e 's/^# \([0-9]*\)/#line \1/' *.i
 sed -i -e 's/internal_function//' *.i
