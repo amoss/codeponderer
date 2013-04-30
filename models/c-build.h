f0a919260ce6b1168a2fcc548a7cf287ae093bdf
Andrew Moss <awm@bth.se>
2013-04-30 17:36:55 +0200

    Removed stale header.

--- models/c-build.h
+++ models/c-build.h
@@ -1,6 +1,19 @@
 #include <list>
-#include "models/c-init.h"
+#include <exception>
+#include "models/util.h"
 #include "models/c-final.h"
 
 TranslationU parseUnit(char *filename);
+
+class BrokenTree : public std::exception
+{
+public:
+  pANTLR3_BASE_TREE blame;
+  const char *explain;
+  BrokenTree(pANTLR3_BASE_TREE node, const char *text)
+    :  blame(node), explain(text)
+  {
+  }
+  const char *what() const throw () { return explain; }
+};
 
